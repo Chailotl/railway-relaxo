01411e0d77f5c42e2924a151f738e988ab4be910
Chai <7232280+VanillaChai@users.noreply.github.com>
2021-12-06 20:23:11 -0500

    Added fancy camera mouse movement

--- Assets/Scripts/Builder.cs
+++ Assets/Scripts/Builder.cs
@@ -6,8 +6,19 @@ public class Builder : MonoBehaviour
 {
 	private Plane floor = new Plane(Vector3.up, Vector3.zero);
 
+	private float rotY = 0f;
+	private float rotX = 0f;
+	private Quaternion foo = Quaternion.identity;
+
 	[SerializeField]
 	private RailwayNetwork network;
+	[SerializeField]
+	private float sensitivity = 1f;
+
+	void Start()
+	{
+		Cursor.lockState = CursorLockMode.Locked;
+	}
 
 	void Update()
 	{
@@ -48,5 +59,16 @@ public class Builder : MonoBehaviour
 		}
 
 		transform.position = pos;
+
+		rotY += Input.GetAxis("Mouse X") * sensitivity;
+		rotX += Input.GetAxis("Mouse Y") * sensitivity;
+
+		rotX = Mathf.Clamp(rotX, -89.9f, -10f);
+
+		Quaternion bar = Quaternion.Euler(-rotX, rotY, 0);
+
+		foo = Quaternion.RotateTowards(foo, bar, Quaternion.Angle(foo, bar) * 7.5f * Time.deltaTime);
+
+		transform.localEulerAngles = foo.eulerAngles;
 	}
 }
